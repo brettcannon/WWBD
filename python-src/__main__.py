59b0692512321e56637fccc01d99b7c95ee526b8
Brett Cannon <brett@python.org>
2022-04-14 10:00:38 -0700

    Guarantee that UTF-8 is used over the pipe

--- python-src/__main__.py
+++ python-src/__main__.py
@@ -1,13 +1,13 @@
 import runpy
+import sys
 
 
 def main(args):
+    sys.__stdout__.reconfigure(encoding="utf-8")
     runpy.run_module("wwbd", alter_sys=True, run_name="__main__")
 
 
 if __name__ == "__main__":
-    import sys
-
     main(sys.argv)
 
 
